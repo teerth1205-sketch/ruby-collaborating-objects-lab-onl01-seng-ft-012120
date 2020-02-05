c1d9fff3c9c083d4b29d8183f010b3d5337019b9
Teerth Patel <teerth1205@gmail.com>
2020-02-05 20:26:52 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+  def initialize(name)
+    @name = name 
+    @@all << self
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all
+  end
+  
+  def add_song(song)
+    @songs << song
+  end 
+  
+  def self.find_or_create_by_name(art)
+    if @@all.find { |names|
+      names.name == art}
+      @@all.find do |names|
+      names.name == art
+    end 
+    else
+    news  = self.new(art) 
+
+    news 
+    end 
+    
+    
+  end 
+  
+  def print_songs
+    @songs.each {|song| puts song.name }
+  end 
+end
---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song 
+  
+  attr_accessor :name, :artist
+  @@all =[]
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.new_by_filename(filename)
+   song = filename.split(" - ")[1]
+    new =  self.new(song)
+    artistname = filename.split(" - ")[0]
+    new.artist_name = artistname
+    new 
+    
+  end
+  
+  def artist_name=(name) 
+   self.artist  = Artist.find_or_create_by_name(name)
+   self.artist.add_song(name)
+  end 
+  
+  
+end 
